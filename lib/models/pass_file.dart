887311e5f55351e1417cb220bb8bd7ba94d6f383
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-02-17 20:37:05 +0300

    Added images to pass file model

--- lib/models/pass_file.dart
+++ lib/models/pass_file.dart
@@ -1,7 +1,15 @@
+import 'package:passkit/models/images/passkit_image.dart';
 import 'package:passkit/models/passkit/passkit_pass.dart';
 
 class PassFile {
   String id;
+
+  PasskitImage background;
+  PasskitImage footer;
+  PasskitImage icon;
+  PasskitImage logo;
+  PasskitImage strip;
+  PasskitImage thumbnail;
 
   PasskitPass pass;
 }
