a6db5305cd48b788d07efc7f8fc373d8c722936e
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-02-27 14:34:47 +0300

    Refactoring  passkit utils class

--- lib/src/passkit_utils.dart
+++ lib/src/passkit_utils.dart
@@ -1,9 +1,9 @@
 part of 'passkit_core.dart';
 
 class _PasskitUtils {
-  static Future<String> generatePathToPass() async {
+  static Future<File> generatePassFile() async {
     Directory passesDir = await _PasskitIo().getPassesDir();
     String passFileName = Uuid().v1() + '.passkit';
-    return '${passesDir.path}/$passFileName';
+    return File('${passesDir.path}/$passFileName');
   }
 }
