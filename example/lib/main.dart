a117868c776a1bf3612271c7a1ab851d9e59c883
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-02-27 17:13:11 +0300

    Rename passkit -> pass_flutter

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:pass_flutter/passkit.dart';
+import 'package:pass_flutter/pass_flutter.dart';
 
 void main() => runApp(MyApp());
 
