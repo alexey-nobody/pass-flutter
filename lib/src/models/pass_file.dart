5dc5f5f5918c2468e675aa91d48830288ceac169
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-02-27 14:28:23 +0300

    Add file and directory to pass

--- lib/src/models/pass_file.dart
+++ lib/src/models/pass_file.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:pass_flutter/src/models/images/passkit_image.dart';
 import 'package:pass_flutter/src/models/passkit/passkit_pass.dart';
 
 class PassFile {
   String id;
+
+  File passFile;
+  Directory passDirectory;
 
   PasskitImage background;
   PasskitImage footer;
