9f92c7067d4a4319911373347ab8112d1abd5abb
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-03-12 22:36:17 +0300

    Add to export all models

--- lib/pass_flutter.dart
+++ lib/pass_flutter.dart
@@ -1,3 +1,11 @@
 library pass_flutter;
 
+
+export 'src/models/pass_file.dart';
+export 'src/models/pass_image.dart';
+export 'src/models/pass_json/pass_json.dart';
+export 'src/models/pass_json/barcode/barcode.dart';
+export 'src/models/pass_json/location/location.dart';
+export 'src/models/pass_json/structure_dictionary/pass_structure_dictionary.dart';
+
 export 'src/pass_core.dart';
