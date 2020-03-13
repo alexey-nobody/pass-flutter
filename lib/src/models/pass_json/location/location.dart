52d069d8679fe400235e50f87731a87ca5315785
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-03-13 15:15:03 +0300

    Add doc for location

--- lib/src/models/pass_json/location/location.dart
+++ lib/src/models/pass_json/location/location.dart
@@ -4,9 +4,17 @@ part 'location.g.dart';
 
 @JsonSerializable()
 class Location {
+  /// Optional. Altitude, in meters, of the location.
   double altitude;
+
+  /// Required. Latitude, in degrees, of the location.
   double latitude;
+
+  /// Required. Longitude, in degrees, of the location.
   double longitude;
+
+  /// Optional. Text displayed on the lock screen when the pass is currently relevant.
+  /// For example, a description of the nearby location such as “Store nearby on 1st and Main.”
   String relevantText;
 
   Location({this.altitude, this.latitude, this.longitude, this.relevantText});
