f1e77aec8de7ee21ff837cf66620d782ba7d9906
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-03-13 15:19:36 +0300

    Add doc for PassStructureDictionary

--- lib/src/models/pass_json/structure_dictionary/pass_structure_dictionary.dart
+++ lib/src/models/pass_json/structure_dictionary/pass_structure_dictionary.dart
@@ -5,12 +5,25 @@ part 'pass_structure_dictionary.g.dart';
 
 @JsonSerializable()
 class PassStructureDictionary {
+  /// Optional. Additional fields to be displayed on the front of the pass.
   List<Fields> auxiliaryFields;
+
+  /// Optional. Fields to be displayed in the header on the front of the pass.
+  /// Use header fields sparingly; unlike all other fields, they remain visible when a stack of passes are displayed.
   List<Fields> headerFields;
+
+  /// Optional. Fields to be displayed on the front of the pass.
   List<Fields> secondaryFields;
+
+  /// Optional. Fields to be on the back of the pass.
   List<Fields> backFields;
+
+  /// Optional. Fields to be displayed prominently on the front of the pass.
   List<Fields> primaryFields;
 
+  /// Required for boarding passes; otherwise not allowed.
+  /// Type of transit. Must be one of the following values:
+  /// PKTransitTypeAir, PKTransitTypeBoat, PKTransitTypeBus, PKTransitTypeGeneric,PKTransitTypeTrain.
   String transitType;
 
   PassStructureDictionary(
