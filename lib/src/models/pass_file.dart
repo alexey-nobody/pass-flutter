5d6b2abf43f7a85ddd7f0ab6ebf48b15e78efa5b
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-03-13 17:12:18 +0300

    Add doc

--- lib/src/models/pass_file.dart
+++ lib/src/models/pass_file.dart
@@ -4,18 +4,35 @@ import 'package:pass_flutter/src/models/pass_image.dart';
 import 'package:pass_flutter/src/models/pass_json/pass_json.dart';
 
 class PassFile {
+  /// Pass file id
   String id;
 
+  /// passkit [File]
   File file;
+
+  /// pass.json [File]
   File passJson;
 
+  /// [Directory] with unpacked pass files
   Directory directory;
 
+  /// The image displayed as the background of the front of the pass.
   PassImage background;
+
+  /// The image displayed on the front of the pass near the barcode.
   PassImage footer;
+
+  /// The pass’s icon. This is displayed in notifications and in emails that have a pass attached, and on the lock screen.
+  /// When it is displayed, the icon gets a shine effect and rounded corners.
   PassImage icon;
+
+  /// The image displayed on the front of the pass in the top left.
   PassImage logo;
+
+  /// The image displayed behind the primary fields on the front of the pass.
   PassImage strip;
+
+  /// An additional image displayed on the front of the pass. For example, on a membership card, the thumbnail could be used to a picture of the cardholder.
   PassImage thumbnail;
 
   PassJson pass;
