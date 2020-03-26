775eb9a89bc3e14226f0c6151117c60ea05ebf33
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-03-26 21:33:01 +0300

    Extend Barcode from Equatable for allow comparisons

--- lib/src/models/pass_json/barcode/barcode.dart
+++ lib/src/models/pass_json/barcode/barcode.dart
@@ -1,25 +1,26 @@
+import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'barcode.g.dart';
 
 @JsonSerializable()
-class Barcode {
+class Barcode extends Equatable {
   /// Optional. Text displayed near the barcode.
   /// For example, a human-readable version of the barcode data in case the barcode doesn’t scan.
-  String altText;
+  final String altText;
 
   /// Required. Barcode format. For the barcode dictionary, you can use only the following values:
   /// PKBarcodeFormatQR, PKBarcodeFormatPDF417, or PKBarcodeFormatAztec.
   /// For dictionaries in the barcodes array, you may also use PKBarcodeFormatCode128.
-  String format;
+  final String format;
 
   /// Required. Message or payload to be displayed as a barcode.
-  String message;
+  final String message;
 
   /// Required. Text encoding that is used to convert the message from the string representation
   /// to a data representation to render the barcode. The value is typically iso-8859-1, but you may
   /// use another encoding that is supported by your barcode scanning infrastructure.
-  String messageEncoding;
+  final String messageEncoding;
 
   Barcode({this.altText, this.format, this.message, this.messageEncoding});
 
@@ -27,4 +28,8 @@ class Barcode {
       _$BarcodeFromJson(json);
 
   Map<String, dynamic> toJson() => _$BarcodeToJson(this);
+
+  @override
+  List<Object> get props =>
+      [this.altText, this.format, this.message, this.messageEncoding];
 }
