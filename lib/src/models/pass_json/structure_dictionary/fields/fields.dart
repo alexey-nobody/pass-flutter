337d051e2f50406bb1a78926f2cec877baeec663
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-03-26 21:46:31 +0300

    add documentation for Fields

--- lib/src/models/pass_json/structure_dictionary/fields/fields.dart
+++ lib/src/models/pass_json/structure_dictionary/fields/fields.dart
@@ -5,9 +5,19 @@ part 'fields.g.dart';
 
 @JsonSerializable()
 class Fields extends Equatable {
+  /// Required. The key must be unique within the scope of the entire pass. For example, “departure-gate.”
   final String key;
+
+  /// Optional. Label text for the field.
   final String label;
+
+  /// Required. Value of the field, for example, 42.
   final String value;
+
+  /// Optional. Format string for the alert text that is displayed when the pass is updated.
+  /// The format string must contain the escape %@, which is replaced with the field’s new value.
+  /// For example, “Gate changed to %@.”
+  /// If you don’t specify a change message, the user isn’t notified when the field changes.
   final String changeMessage;
 
   Fields({
