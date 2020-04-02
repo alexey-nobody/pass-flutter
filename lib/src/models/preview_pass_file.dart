c57989e9fbdd0708ac2e28a35a538ae5de8363a4
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-04-02 20:47:32 +0300

    Add method for saving preview to internal memory

--- lib/src/models/preview_pass_file.dart
+++ lib/src/models/preview_pass_file.dart
@@ -2,9 +2,15 @@ import 'dart:io';
 
 import 'package:pass_flutter/pass_flutter.dart';
 import 'package:pass_flutter/src/models/pass_file.dart';
+import 'package:pass_flutter/src/pass_core.dart';
 
 /// Preview of parsed Apple Wallet pass file
 class PreviewPassFile extends PassFile {
+  /// Save current PassFile to internal memory
+  Future<PassFile> save() async {
+    return await PassIo().saveFromPath(externalPassFile: this.file);
+  }
+
   /// Create a new [PreviewPassFile]
   PreviewPassFile(
     String id,
