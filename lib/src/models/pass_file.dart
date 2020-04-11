1e5e5d8851d008a24d7515970a223e8eb548f7d3
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-04-11 14:30:43 +0300

    Finalize all fieilds in PassFile

--- lib/src/models/pass_file.dart
+++ lib/src/models/pass_file.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:pass_flutter/src/models/pass_image.dart';
 import 'package:pass_flutter/src/models/pass_json/pass_json.dart';
 import 'package:pass_flutter/src/pass_core.dart';
@@ -7,42 +8,49 @@ import 'package:pass_flutter/src/pass_core.dart';
 /// Parsed Apple Wallet pass file
 class PassFile {
   /// Pass file id
-  String id;
+  final String id;
 
   /// passkit [File]
-  File file;
+  final File file;
 
   /// [Directory] with unpacked pass files
-  Directory directory;
+  final Directory directory;
 
   /// The image displayed as the background of the front of the pass.
-  PassImage background;
+  final PassImage background;
 
   /// The image displayed on the front of the pass near the barcode.
-  PassImage footer;
+  final PassImage footer;
 
   /// The pass’s icon. This is displayed in notifications and in emails that have a pass attached, and on the lock screen.
   /// When it is displayed, the icon gets a shine effect and rounded corners.
-  PassImage icon;
+  final PassImage icon;
 
   /// The image displayed on the front of the pass in the top left.
-  PassImage logo;
+  final PassImage logo;
 
   /// The image displayed behind the primary fields on the front of the pass.
-  PassImage strip;
+  final PassImage strip;
 
   /// An additional image displayed on the front of the pass. For example, on a membership card, the thumbnail could be used to a picture of the cardholder.
-  PassImage thumbnail;
+  final PassImage thumbnail;
 
   /// A JSON dictionary that defines the pass.
-  PassJson pass;
+  final PassJson pass;
 
   /// Creates a new instance of [PassFile]
-  PassFile(String id, File passFile, Directory passDirectory) {
-    this.id = id;
-    this.file = passFile;
-    this.directory = passDirectory;
-  }
+  PassFile({
+    @required this.id,
+    @required this.file,
+    @required this.directory,
+    this.background,
+    this.footer,
+    this.icon,
+    this.logo,
+    this.strip,
+    this.thumbnail,
+    @required this.pass,
+  }) : assert(id != null && file != null, directory != null && pass != null);
 
   /// Delete current pass file from memory
   void delete() {
